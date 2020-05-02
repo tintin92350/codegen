2343d5a60399da62b3f2bc11bd2e82902690e029
Quentin RODIC <rodic.quentin.corporation@gmail.com>
2020-05-02 19:55:55 +0200

    Adding remake command and help section

---
+++ test/test.c
@@ -0,0 +1,16 @@
+/**
+ * name: test.c
+ * description : Main entry point of the program
+ * creation : 2/5/2020 at 19:55:22
+ * author : 
+*/
+
+// Standard library
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv)
+{
+	printf("hello world\n");
+	return EXIT_SUCCESS;
+}
